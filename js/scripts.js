ef159bb10b15b5bb153fec30061071a19011111c
Max Larson, Rob McKenzie and Tom Yoder <rob@>
2017-02-27 16:43:06 -0800

    add styling to list stuff

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,7 @@ function toDo(itemTask, itemStartDate, itemStartTime) {
   this.itemStartTime=itemStartTime;
 }
 toDo.prototype.toDoTask = function () {
-  return  "<li class= \"" + this.itemTask + "\" >" +"<span class='caret'></span>" + "Task: " + this.itemTask +   "</li>" + "<li>" + "Date: " + this.itemStartDate + "</li>" + "<li>" + "Time: " + this.itemStartTime  + "</li>";
+  return  "<li class= \"" + this.itemTask + "\" >" + "Task: " + this.itemTask +   "</li>" + "<li class= \"" + this.itemStartDate + "\">" + "Date: " + this.itemStartDate + "</li>" + "<li class= \"" + this.itemStartDate + "\">" + "Time: " + this.itemStartTime  + "</li>";
 }
 
 
@@ -22,10 +22,14 @@ $(document).ready(function () {
     var newTask= new toDo(userInput1,userInput2,userInput3);
     $("#events").append(newTask.toDoTask());
     $("#events").addClass("listItem");
-
+    $("." + newTask.itemTask).addClass("superTask");
+    $("." + newTask.itemStartDate).addClass("dateStuff");
     $("li." + newTask.itemTask ).click(function(event){
+
       event.preventDefault();
-    $("." + newTask.itemTask ).toggleClass("done");
+
+      $("." + newTask.itemStartDate).toggle();
+
     });
   });
 
